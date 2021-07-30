dd848c82b9dd122fd04825f5c791625e2abdc009
Sean Moulton <34073738+WindowsVista42@users.noreply.github.com>
2021-07-29 23:42:06 -0500

    vulkan buffer creation helper functions

--- src/vkutil.h
+++ src/vkutil.h
@@ -67,5 +67,131 @@ void create_shader_module(VkDevice device, const char* buffer, usize buffer_size
     }
 }
 
+u32 find_memory_type(VkPhysicalDevice physical_device, u32 type_filter, VkMemoryPropertyFlags properties) {
+    VkPhysicalDeviceMemoryProperties memory_properties;
+    vkGetPhysicalDeviceMemoryProperties(physical_device, &memory_properties);
+
+    for(u32 index = 0; index < memory_properties.memoryTypeCount; index += 1) {
+        if((type_filter & (1 << index)) && (memory_properties.memoryTypes[index].propertyFlags & properties) == properties) {
+            return index;
+        }
+    }
+
+    panic("Failed to find suitable memory type!");
+    return UINT32_MAX;
+}
+
+void _create_buffer(
+    VkDevice device,
+    VkPhysicalDevice physical_device,
+    VkDeviceSize size,
+    VkBufferUsageFlags usage,
+    VkMemoryPropertyFlags properties,
+    VkBuffer* buffer,
+    VkDeviceMemory* buffer_memory
+) {
+    VkBufferCreateInfo buffer_create_info = {};
+    buffer_create_info.sType = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO;
+    buffer_create_info.size = size;
+    buffer_create_info.usage = usage;
+    buffer_create_info.sharingMode = VK_SHARING_MODE_EXCLUSIVE;
+
+    if(vkCreateBuffer(device, &buffer_create_info, 0, buffer) != VK_SUCCESS) {
+        panic("Failed to create vertex buffer!");
+    }
+
+    VkMemoryRequirements memory_requirements;
+    vkGetBufferMemoryRequirements(device, *buffer, &memory_requirements);
+
+    VkMemoryAllocateInfo allocate_info = {};
+    allocate_info.sType = VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_INFO;
+    allocate_info.allocationSize = memory_requirements.size;
+    allocate_info.memoryTypeIndex = find_memory_type(physical_device, memory_requirements.memoryTypeBits, properties);
+
+    if(vkAllocateMemory(device, &allocate_info, 0, buffer_memory) != VK_SUCCESS) {
+        panic("Failed to allocate vertex buffer!");
+    }
+
+    vkBindBufferMemory(device, *buffer, *buffer_memory, 0);
+}
+
+void copy_buffer_to_buffer(
+    VkQueue queue,
+    VkDevice device,
+    VkCommandPool command_pool,
+    VkBuffer src,
+    VkBuffer dst,
+    VkDeviceSize size
+) {
+    VkCommandBufferAllocateInfo allocate_info = {};
+    allocate_info.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO;
+    allocate_info.level = VK_COMMAND_BUFFER_LEVEL_PRIMARY;
+    allocate_info.commandPool = command_pool;
+    allocate_info.commandBufferCount = 1;
+
+    VkCommandBuffer command_buffer;
+    vkAllocateCommandBuffers(device, &allocate_info, &command_buffer);
+
+    VkCommandBufferBeginInfo begin_info = {};
+    begin_info.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
+    begin_info.flags = 0;
+    begin_info.pInheritanceInfo = 0;
+
+    vkBeginCommandBuffer(command_buffer, &begin_info);
+
+        VkBufferCopy copy_region = {};
+        copy_region.srcOffset = 0;
+        copy_region.dstOffset = 0;
+        copy_region.size = size;
+
+        vkCmdCopyBuffer(command_buffer, src, dst, 1, &copy_region);
+
+    vkEndCommandBuffer(command_buffer);
+
+    VkSubmitInfo submit_info = {};
+    submit_info.sType = VK_STRUCTURE_TYPE_SUBMIT_INFO;
+    submit_info.commandBufferCount = 1;
+    submit_info.pCommandBuffers = &command_buffer;
+
+    vkQueueSubmit(queue, 1, &submit_info, 0);
+    vkQueueWaitIdle(queue);
+
+    vkFreeCommandBuffers(device, command_pool, 1, &command_buffer);
+}
+
+void create_device_local_buffer(
+    VkDevice device,
+    VkPhysicalDevice physical_device,
+    VkQueue queue,
+    VkCommandPool command_pool,
+    VkDeviceSize size,
+    void* data,
+    VkBufferUsageFlags usage,
+    VkBuffer* buffer,
+    VkDeviceMemory* buffer_memory
+) {
+    VkBuffer staging_buffer;
+    VkDeviceMemory staging_buffer_memory;
+    _create_buffer(device, physical_device, size,
+                   VK_BUFFER_USAGE_TRANSFER_SRC_BIT,
+                   VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT,
+                   &staging_buffer, &staging_buffer_memory);
+
+    void* mapped_data;
+    vkMapMemory(device, staging_buffer_memory, 0, size, 0, &mapped_data);
+    memcpy(mapped_data, data, (usize)size);
+    vkUnmapMemory(device, staging_buffer_memory);
+
+    _create_buffer(device, physical_device, size,
+                   VK_BUFFER_USAGE_TRANSFER_DST_BIT | usage,
+                   VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT,
+                   buffer, buffer_memory);
+
+    copy_buffer_to_buffer(queue, device, command_pool, staging_buffer, *buffer, size);
+
+    vkDestroyBuffer(device, staging_buffer, 0);
+    vkFreeMemory(device, staging_buffer_memory, 0);
+}
+
 #define UNTITLED_FPS_VKUTIL_H
 #endif //UNTITLED_FPS_VKHELPERS_H
