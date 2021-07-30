da0e695cb26f282b7fc71af219c1297ec4d53402
Sean Moulton <34073738+WindowsVista42@users.noreply.github.com>
2021-07-29 23:43:17 -0500

    added required matrix math for view projection generation

--- src/vmmath.h
+++ src/vmmath.h
@@ -55,39 +55,138 @@ global inline vec3 vec3_norm(vec3 vector);
 global inline vec3 vec3_cross(vec3 lhs, vec3 rhs);
 global inline f32 vec3_dot(vec3 lhs, vec3 rhs);
 
-global inline vec2 add_vec2(vec2 lhs, vec2 rhs) {
-    vec2 ret = {{lhs.x + rhs.x, lhs.y + rhs.y}};
-    return ret;
+global inline mat4 mat4_splat(f32 splat) {
+    mat4 matrix = {{
+        splat, splat, splat, splat,
+        splat, splat, splat, splat,
+        splat, splat, splat, splat,
+        splat, splat, splat, splat,
+    }};
+    return matrix;
 }
 
-global inline vec2 sub_vec2(vec2 lhs, vec2 rhs) {
-    vec2 ret = {{lhs.x - rhs.x, lhs.y - rhs.y}};
-    return ret;
+global inline vec3 vec3_mul_scalar(vec3 vector, f32 scalar) {
+    vec3 output;
+    output.x = vector.x * scalar;
+    output.y = vector.y * scalar;
+    output.z = vector.z * scalar;
+    return output;
 }
 
-global inline vec2 mul_vec2(vec2 lhs, vec2 rhs) {
-    vec2 ret = {{lhs.x * rhs.x, lhs.y * rhs.y}};
-    return ret;
+global inline vec3 vec3_mul_vec3(vec3 lhs, vec3 rhs) {
+    vec3 output;
+    output.x = lhs.x * rhs.x;
+    output.y = lhs.y * rhs.y;
+    output.z = lhs.z * rhs.z;
+    return output;
 }
 
-global inline vec2 div_vec2(vec2 lhs, vec2 rhs) {
-    vec2 ret = {{lhs.x / rhs.x, lhs.y / rhs.y}};
-    return ret;
+global inline vec4 vec4_unit_w() {
+    vec4 output = {{0.0f, 0.0f, 0.0f, 1.0f}};
+    return output;
 }
 
-global inline f32 dotprod_vec2(vec2 lhs, vec2 rhs) {
-    return (lhs.x * rhs.x) + (lhs.y * rhs.y);
+global inline mat4 mat4_rotate(mat4 matrix, f32 angle, vec3 axis) {
+    f32 sin = sinf(angle);
+    f32 cos = cosf(angle);
+    vec3 axis_sin = vec3_mul_scalar(axis, sin);
+    vec3 axis_sq = vec3_mul_vec3(axis, axis);
+    f32 omc = 1.0 - cos;
+    f32 xyomc = axis.x * axis.y * omc;
+    f32 xzomc = axis.x * axis.z * omc;
+    f32 yzomc = axis.y * axis.z * omc;
+
+    mat4 output = {
+        .xs = {{
+            axis_sq.x * omc + cos,
+            xyomc + axis_sin.z,
+            xzomc - axis_sin.y,
+            0.0f,
+        }},
+        .ys = {{
+            xyomc - axis_sin.z,
+            axis_sq.y * omc + cos,
+            yzomc + axis_sin.x,
+            0.0f,
+        }},
+        .zs = {{
+            xzomc + axis_sin.y,
+            yzomc - axis_sin.x,
+            axis_sq.z * omc + cos,
+            0.0f,
+        }},
+        .ws = vec4_unit_w(),
+    };
+
+    return output;
 }
 
-global inline f32 mag_vec2(vec2 lhs) {
-    return sqrtf((lhs.x * lhs.x) + (lhs.y * lhs.y));
+global inline vec3 vec3_sub_vec3(vec3 lhs, vec3 rhs) {
+    vec3 output = {{
+        lhs.x - rhs.x,
+        lhs.y - rhs.y,
+        lhs.z - rhs.z
+    }};
+    return output;
 }
 
-/// Squared magnitude of lhs
-global inline f32 sqmag_vec2(vec2 lhs) {
-    return (lhs.x * lhs.x) + (lhs.y * lhs.y);
+global inline f32 vec3_length(vec3 vector) {
+    return sqrtf(vec3_dot(vector, vector));
 }
 
+global inline f32 vec3_length_recip(vec3 vector) {
+    return 1.0 / vec3_length(vector);
+}
+
+global inline vec3 vec3_norm(vec3 vector) {
+    return vec3_mul_scalar(vector, vec3_length_recip(vector));
+}
+
+global inline vec3 vec3_cross(vec3 lhs, vec3 rhs) {
+    vec3 output = {{
+        lhs.y * rhs.z - rhs.y * lhs.z,
+        lhs.z * rhs.x - rhs.z * lhs.x,
+        lhs.x * rhs.y - rhs.x * lhs.y
+    }};
+    return output;
+}
+
+global inline f32 vec3_dot(vec3 lhs, vec3 rhs) {
+    return (lhs.x * rhs.x) + (lhs.y * rhs.y) + (lhs.z * rhs.z);
+}
+
+global inline mat4 mat4_look_at(vec3 eye, vec3 center, vec3 up) {
+    vec3 dir = vec3_sub_vec3(eye, center);
+    vec3 f = vec3_norm(dir);
+    vec3 s = vec3_norm(vec3_cross(up, f));
+    vec3 u = vec3_cross(f, s);
+
+    mat4 output = {
+        .xs = {{s.x, u.x, f.x, 0.0f}},
+        .ys = {{s.y, u.y, f.y, 0.0f}},
+        .zs = {{s.z, u.z, f.z, 0.0f}},
+        .ws = {{-vec3_dot(s, eye), -vec3_dot(u, eye), -vec3_dot(f, eye), 1.0f}},
+    };
+
+    return output;
+}
+
+global inline mat4 mat4_perspective(f32 fov, f32 asp, f32 z_near, f32 z_far) {
+    f32 inv_length = 1.0f / (z_near - z_far);
+    f32 f = 1.0f / tanf((0.5f * fov));
+    f32 a = f / asp;
+    f32 b = (z_near + z_far) * inv_length;
+    f32 c = (2.0f * z_near * z_far) * inv_length;
+
+    mat4 output = {
+        .xs = {{a,    0.0f, 0.0f,  0.0f}},
+        .ys = {{0.0f, f,    0.0f,  0.0f}},
+        .zs = {{0.0f, 0.0f, b,    -1.0f}},
+        .ws = {{0.0f, 0.0f, c,     0.0f}},
+    };
+
+    return output;
+}
 
 #define UNTITLED_FPS_VMMATH_H
 
