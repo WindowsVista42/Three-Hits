0a8230209fb612a0a336b65bdd1d4e3b994cadb8
Sean Moulton <bananamoulton@gmail.com>
2021-08-07 18:52:23 -0500

    Fixed transition_image_layout

--- src/vkutil.h
+++ src/vkutil.h
@@ -184,10 +184,8 @@ void transition_image_layout(
         barrier.newLayout = new_layout;
         barrier.srcQueueFamilyIndex = VK_QUEUE_FAMILY_IGNORED;
         barrier.dstQueueFamilyIndex = VK_QUEUE_FAMILY_IGNORED;
-
         barrier.image = image;
         barrier.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
-
         barrier.subresourceRange.baseMipLevel = 0;
         barrier.subresourceRange.levelCount = 1;
         barrier.subresourceRange.baseArrayLayer = 0;
@@ -196,15 +194,15 @@ void transition_image_layout(
         if(old_layout == VK_IMAGE_LAYOUT_UNDEFINED && new_layout == VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL) {
             barrier.srcAccessMask = 0;
             barrier.dstAccessMask = VK_ACCESS_TRANSFER_WRITE_BIT;
-
             src_stage_flags = VK_PIPELINE_STAGE_TOP_OF_PIPE_BIT;
             dst_stage_flags = VK_PIPELINE_STAGE_TRANSFER_BIT;
+
         } else if (old_layout == VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL && new_layout == VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL) {
             barrier.srcAccessMask = VK_ACCESS_TRANSFER_WRITE_BIT;
             barrier.dstAccessMask = VK_ACCESS_SHADER_READ_BIT;
-
             src_stage_flags = VK_PIPELINE_STAGE_TRANSFER_BIT;
             dst_stage_flags = VK_PIPELINE_STAGE_FRAGMENT_SHADER_BIT;
+
         } else {
             panic("Unsupported layout transition!");
         }
