ece41446d3b1c8c4c18f8abec5aa419bfbc9812e
Sean Moulton <34073738+WindowsVista42@users.noreply.github.com>
2021-08-18 17:19:06 -0500

    Data

--- build.bat
+++ build.bat
@@ -1,8 +1,8 @@
-C:/VulkanSDK/1.2.182.0/Bin/glslc.exe src/level.vert -o level.vert.spv
-C:/VulkanSDK/1.2.182.0/Bin/glslc.exe src/level.frag -o level.frag.spv
+C:/VulkanSDK/1.2.182.0/Bin/glslc.exe src/level.vert -o data/shaders/level.vert.spv
+C:/VulkanSDK/1.2.182.0/Bin/glslc.exe src/level.frag -o data/shaders/level.frag.spv
 
-C:/VulkanSDK/1.2.182.0/Bin/glslc.exe src/enemy.vert -o enemy.vert.spv
-C:/VulkanSDK/1.2.182.0/Bin/glslc.exe src/enemy.frag -o enemy.frag.spv
+C:/VulkanSDK/1.2.182.0/Bin/glslc.exe src/enemy.vert -o data/shaders/enemy.vert.spv
+C:/VulkanSDK/1.2.182.0/Bin/glslc.exe src/enemy.frag -o data/shaders/enemy.frag.spv
 
-C:/VulkanSDK/1.2.182.0/Bin/glslc.exe src/crosshair.vert -o crosshair.vert.spv
-C:/VulkanSDK/1.2.182.0/Bin/glslc.exe src/crosshair.frag -o crosshair.frag.spv
+C:/VulkanSDK/1.2.182.0/Bin/glslc.exe src/crosshair.vert -o data/shaders/crosshair.vert.spv
+C:/VulkanSDK/1.2.182.0/Bin/glslc.exe src/crosshair.frag -o data/shaders/crosshair.frag.spv
