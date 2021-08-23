2fdac9a9c76035b30839962674e93f5b08cd8b24
Sean Moulton <34073738+WindowsVista42@users.noreply.github.com>
2021-08-23 09:32:03 -0500

    updates

---
+++ build.bat
@@ -0,0 +1,8 @@
+C:/VulkanSDK/1.2.182.0/Bin/glslc.exe src/level.vert -o data/shaders/level.vert.spv
+C:/VulkanSDK/1.2.182.0/Bin/glslc.exe src/level.frag -o data/shaders/level.frag.spv
+
+C:/VulkanSDK/1.2.182.0/Bin/glslc.exe src/entity.vert -o data/shaders/entity.vert.spv
+C:/VulkanSDK/1.2.182.0/Bin/glslc.exe src/entity.frag -o data/shaders/entity.frag.spv
+
+C:/VulkanSDK/1.2.182.0/Bin/glslc.exe src/crosshair.vert -o data/shaders/crosshair.vert.spv
+C:/VulkanSDK/1.2.182.0/Bin/glslc.exe src/crosshair.frag -o data/shaders/crosshair.frag.spv
