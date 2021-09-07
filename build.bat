2167376f75e22bb7f245e223952f7e59f1d15c28
Sean Moulton <34073738+WindowsVista42@users.noreply.github.com>
2021-09-07 15:17:18 -0500

    Add compilation commands for hudelement shaders

--- build.bat
+++ build.bat
@@ -4,5 +4,5 @@ C:/VulkanSDK/1.2.182.0/Bin/glslc.exe src/level.frag -o data/shaders/level.frag.s
 C:/VulkanSDK/1.2.182.0/Bin/glslc.exe src/entity.vert -o data/shaders/entity.vert.spv
 C:/VulkanSDK/1.2.182.0/Bin/glslc.exe src/entity.frag -o data/shaders/entity.frag.spv
 
-C:/VulkanSDK/1.2.182.0/Bin/glslc.exe src/crosshair.vert -o data/shaders/crosshair.vert.spv
-C:/VulkanSDK/1.2.182.0/Bin/glslc.exe src/crosshair.frag -o data/shaders/crosshair.frag.spv
+C:/VulkanSDK/1.2.182.0/Bin/glslc.exe src/hudelement.vert -o data/shaders/hudelement.vert.spv
+C:/VulkanSDK/1.2.182.0/Bin/glslc.exe src/hudelement.frag -o data/shaders/hudelement.frag.spv
